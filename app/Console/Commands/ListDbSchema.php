244732a0f1a7d8a37072122727eb60ee0519d52b
andresgutgon <andresgutgon@gmail.com>
2023-10-01 19:20:36 +0200

    Implement a table of output for each table in the db with table name, column name, column type and if it's required

--- app/Console/Commands/ListDbSchema.php
+++ app/Console/Commands/ListDbSchema.php
@@ -3,20 +3,30 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class ListDbSchema extends Command
 {
     protected $excluded_table_names = [
         'migrations',
-        'password_reset_tokens',
         'failed_jobs',
         'personal_access_tokens',
     ];
 
+    protected $table_headers = ['table', 'column', 'type', 'required'];
+
     protected $signature = 'app:db_schema';
 
+    protected $schema_manager;
+
     protected $description = 'List the tables of the app and their fields';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->schema_manager = DB::connection()->getDoctrineSchemaManager();
+    }
 
     protected function allTables(): array
     {
@@ -30,11 +40,30 @@ class ListDbSchema extends Command
         });
     }
 
+    /**
+     * This is a sample function that adds two numbers.
+     *
+     * @param  string  $table
+     */
+    protected function printTable($table): void
+    {
+        $name = $table->tablename;
+        $columns = Schema::getColumnListing($name);
+        $data = array_map(function ($column) use ($name) {
+            $details = $this->schema_manager->listTableDetails($name);
+            $type = Schema::getColumnType($name, $column);
+            $is_nullable = $details->getColumn($column)->getNotnull();
+
+            return [$name, $column, $type, $is_nullable];
+        }, $columns);
+
+        $this->table($this->table_headers, $data);
+    }
+
     public function handle(): void
     {
         foreach ($this->businessTables() as $table) {
-            $this->info($table->tablename);
+            $this->printTable($table);
         }
-        $this->info('The command was successful!');
     }
 }
