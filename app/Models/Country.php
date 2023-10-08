ae4ccbfcc097be3222d6e0dd7f3e9c4d39535595
andresgutgon <andresgutgon@gmail.com>
2023-10-08 18:43:30 +0200

    Create organizations table

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     protected $fillable = ['name', 'code', 'emoji', 'dial_code'];
+
+    public function locations(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
 }
