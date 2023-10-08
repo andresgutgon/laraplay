d8d74019205cf45bc21f29481443fa3a2aecbbbe
andresgutgon <andresgutgon@gmail.com>
2023-10-08 18:30:27 +0200

    Add locations table

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,11 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 // TODO: Use reverse geocoding to facilitate
 // location filling
 // https://nominatim.openstreetmap.org/search?q=aribau%2022%20barcelona&limit=5&format=json&addressdetails=1
 class Location extends Model
 {
-    protected $fillable = ['city'];
+    protected $fillable = [
+        'city',
+        'country_id',
+        'display_name',
+        'road',
+        'house_number',
+        'state',
+        'state_district',
+        'lat',
+        'lon',
+    ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
---
+++ database/migrations/2023_10_08_160920_create_locations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('city');
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('id')->on('countries');
+
+            // Optional
+            $table->string('display_name')->nullable();
+            $table->string('road')->nullable();
+            $table->string('house_number')->nullable();
+            $table->string('state')->nullable();
+            $table->string('state_district')->nullable();
+            $table->integer('lat')->nullable();
+            $table->integer('lon')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
