4a2d7b0863c5e7bcd3b0394bbb98f3efd7c3799a
andresgutgon <andresgutgon@gmail.com>
2023-10-14 09:26:05 +0200

    Add slug field to organization

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Organization extends Model
 {
+    use Sluggable;
+
     protected $fillable = [
         'name',
         'location_id',
@@ -15,5 +18,13 @@ class Organization extends Model
     public function location(): BelongsTo
     {
         return $this->belongsTo(Location::class);
+    }
+
+    /**
+     * @return array<string, array<string, string>>
+     */
+    public function sluggable(): array
+    {
+        return ['slug' => ['source' => 'name']];
     }
 }
--- database/migrations/2023_10_08_163723_create_organizations_table.php
+++ database/migrations/2023_10_08_163723_create_organizations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->unsignedBigInteger('location_id');
             $table->foreign('location_id')->references('id')->on('locations');
 
