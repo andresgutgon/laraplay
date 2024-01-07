4c4548883a7e62a38d94a8cdcbcf12a4f70f2173
andresgutgon <andresgutgon@gmail.com>
2024-01-07 13:35:58 +0100

    Setup the bases for a frontend architecture with livewire and alpineJS I could use [wireui](https://github.com/wireui/wireui) looks pretty good but I want to accomplish two things doing it all from scratch. First I want to learn more about Livewire and AlpineJs and secondly I want to have full control on the look and feel of my components. Also these kind of full UI libraries have a lot of configuration options that I might not need.

--- resources/views/components/ds/text/h4.blade.php
+++ resources/views/components/ds/text/h4.blade.php
@@ -2,6 +2,6 @@
   'weight' => null,
   'color' => null,
 ])
-<x-ds.text.base {{ $attributes }} font='h4' :weight=$weight>
+<x-ds.text.base {{ $attributes }} font='h4' :weight=$weight :color=$color>
   {{ $slot }}
 </x-ds.text.base>
